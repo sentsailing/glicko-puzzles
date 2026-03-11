215a98fde10820fbcdd13d332ea28436215cde5c
David Wang <d4vidwang@gmail.com>
2026-03-10 17:28:54 -0700

    Revert "style: move rating column closer to username"
    This reverts commit c61d73fddf9d8e4aa2e1267e0f7006d611719ae9.

--- src/app/leaderboard/page.tsx
+++ src/app/leaderboard/page.tsx
@@ -78,7 +78,7 @@ function LeaderboardRow({
       </div>
 
       {/* Player info */}
-      <div className="flex-1 min-w-0 max-w-[45%]">
+      <div className="flex-1 min-w-0">
         <span
           className={`text-sm font-semibold truncate ${isMedal ? "" : tier.color}`}
           style={isMedal
